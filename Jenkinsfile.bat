91707043d2a9a4dd516437225dae6c2447cbe669
rooneybb <rooneybb2@gmail.com>
2016-04-25 21:21:28 -0500

    add jenkinsfile
    add jenkinsfile

---
+++ Jenkinsfile.bat
@@ -0,0 +1,17 @@
+﻿stage "se441-qotd-pipeline"
+
+
+
+node {
+
+git 'https://github.com/rooneybb/se441-qotd-pipeline.git'
+
+
+
+def gradleHome = tool 'gradle-2.11'
+
+bat "${gradleHome}\\bin\\gradle.bat assemble uploadArchives"
+
+step([$class: 'ArtifactArchiver', artifacts: '**/*.war', fingerprint: true])
+ 
+}
